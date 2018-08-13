e697a153e251aad30a15eca8c91de2bf0f548533
fitharwe <jesterenwalker@gmail.com>
2018-08-12 21:54:54 -0400

    adding file structure and content

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var friendList = [
+    {
+      name: 'Jessica Steren',
+      profilePic: 'https://avatars3.githubusercontent.com/u/36463819?s=400&u=a56985b2db3abfda437a666696f46f6ced871efa&v=4',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
+  
